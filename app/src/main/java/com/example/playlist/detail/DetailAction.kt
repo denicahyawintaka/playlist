34bcedfcb9069da1cf2a5159e4f5bde920aa6750
denicahyawintaka <denicahyawintaka@gmail.com>
2020-04-30 15:41:10 +0700

    test push

--- app/src/main/java/com/example/playlist/detail/DetailAction.kt
+++ app/src/main/java/com/example/playlist/detail/DetailAction.kt
@@ -2,4 +2,5 @@ package com.example.playlist.detail
 
 sealed class DetailAction {
     data class UpdateStartPositionAction(val playWhenReady: Boolean, val startWindow:Int, val startPosition:Long) : DetailAction()
+
 }
